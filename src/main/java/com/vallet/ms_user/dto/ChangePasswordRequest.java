31748d3ff30d9c727483a919e0db62873630e547
Rémy VALLET <remy.vallet@gmail.com>
2025-06-25 16:31:09 +0200

    Updates DTOs with Swagger annotations
    Enhances API documentation by adding Swagger annotations to DTOs.
    
    These annotations provide clear descriptions and examples for each field,
    improving the discoverability and usability of the API for developers.
    Login field in ChangePasswordRequest and UserDto is now email.
    Password regex is now displayed with the default value.

--- src/main/java/com/vallet/ms_user/dto/ChangePasswordRequest.java
+++ src/main/java/com/vallet/ms_user/dto/ChangePasswordRequest.java
@@ -4,22 +4,25 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import static com.vallet.ms_user.service.impl.UtilisateurServiceImpl.DEFAULT_PASSWORD_REGEX;
 
+/**
+ * Représente une requête pour changer le mot de passe d'un utilisateur.
+ * Contient les informations nécessaires pour identifier l'utilisateur et le nouveau mot de passe.
+ */
 public class ChangePasswordRequest {
 
-    @Schema(description = "Login de l'utilisateur",
-            example = "johndoe",
+    @Schema(description = "email de l'utilisateur",
+            example = "johndoe@email.com",
             requiredMode = Schema.RequiredMode.REQUIRED)
     private String login;
 
-    @Schema(description = "Nouveau mot de passe",
-            example = "newSecurePassword123",
+    @Schema(description = "Nouveau mot de passe respectant les règles définies par l'expression régulière",
+            example = "newSecurePassword@123",
             requiredMode = Schema.RequiredMode.REQUIRED)
     private String newPassword;
 
-    @Schema(description = "Expression régulière pour valider le mot de passe (optionnelle)",
-            example = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$",
-            requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-            defaultValue = DEFAULT_PASSWORD_REGEX)
+    @Schema(description = "Expression régulière pour valider le mot de passe (optionnelle, applique la validation par défaut en example si non spécifiée)",
+            example = DEFAULT_PASSWORD_REGEX,
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String passwordRegex;
 
     public ChangePasswordRequest() {
